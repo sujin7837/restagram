9729cb1dd247fd1e251dc99e98f34949b4d499de
chaeyoung <codud7239@gmail.com>
2020-12-19 00:49:53 +0900

    Initial commit

---
+++ src/main/java/com/example/restagram/RestagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.restagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restagram/RestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
